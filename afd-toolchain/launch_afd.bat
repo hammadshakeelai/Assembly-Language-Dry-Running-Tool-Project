a489e306b750b36259e55f6258747c991b00236b
hammadai <hammadshakeel61@gmail.com>
2026-09-07 00:15:43 +0500

    Create launch_afd.bat

---
+++ afd-toolchain/launch_afd.bat
@@ -0,0 +1,3 @@
+@echo off
+start "" "C:\Program Files (x86)\DOSBox-0.74-3\DOSBox.exe" -conf "C:\Users\HP\Documents\GitHub\assembly-lowlevel\Assembly Language Dry Running Tool Project\afd-toolchain\run_afd.conf"
+exit
